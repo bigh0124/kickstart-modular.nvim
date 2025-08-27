96e4e8adf98de7a7af2d24a054e070a96b70373f
bigh0124 <thubgi0629@gmail.com>
2025-08-27 12:03:47 +0800

    add github_dark_theme

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,13 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+
+return {
+  {
+    'projekt0n/github-nvim-theme',
+    priority = 1000,
+    config = function()
+      vim.cmd 'colorscheme github_dark_default'
+    end,
+  },
+}
